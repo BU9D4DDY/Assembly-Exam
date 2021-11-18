9965434219d9e98fcc44c3f6024da504864dafd2
Diaa Hassan <diaa180409@feng.bu.edu.eg>
2021-11-18 05:54:36 +0200

    commit

---
+++ src/ExamCode.asm
@@ -0,0 +1,37 @@
+.model small 
+
+
+.data
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+header DB "- Assembly Exam -",0Dh,0Ah
+       DB "*****************",'$' 
+       
+NEWLINE    DB 10,13,"$"
+      
+yes_or_no  DB 0Dh,0Ah,0Dh,0Ah,"Enter y or n",0Dh,0Ah,10,13,'$'
+q1         DB "Assembly is a low level programming language? ",'$'
+q2         DB 0Dh,0Ah,"Array are not used in Assembly language? ",'$'
+q3         DB 0Dh,0Ah,"Number can be signed or unsigned? ",'$' 
+correct    DB 0Dh,0Ah,"Number of correct answers are: ",'$'
+allright   DB 0Dh,0Ah,"All your answer are correct",0Dh,0Ah,'$'
+noright    DB 0Dh,0Ah,"No right answer",'$'
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.code
+;;;;;;;;;;;;;;;; Printing Function;;;;;;;;;;;;;;;;;;;;;
+PRINT macro msg
+    
+    pusha
+    pushf
+      
+    LEA DX,msg  
+    MOV AH,09H
+    INT 21H
+    
+    popf
+    popa  
+    
+    endm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
