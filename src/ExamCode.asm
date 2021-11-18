3a5e2f6fd9b627d05563112c73582fb7f5169379
Diaa Hassan <diaa180409@feng.bu.edu.eg>
2021-11-18 05:57:05 +0200

    final commit

--- src/ExamCode.asm
+++ src/ExamCode.asm
@@ -19,6 +19,7 @@ allright   DB 0Dh,0Ah,"All your answer are correct",0Dh,0Ah,'$'
 noright    DB 0Dh,0Ah,"No right answer",'$'
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 .code
 ;;;;;;;;;;;;;;;; Printing Function;;;;;;;;;;;;;;;;;;;;;
 PRINT macro msg
@@ -34,4 +35,83 @@ PRINT macro msg
     popa  
     
     endm
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;       
+main proc
+    
+    mov ax,@data
+    mov ds,ax
+    
+    PRINT header
+    PRINT NEWLINE
+    PRINT yes_or_no
+     
+    mov bl, 0       ;to track correct answer
+    
+    PRINT q1
+    call UserInputY
+    PRINT q2
+    call UserInputN
+    PRINT q3
+    call UserInputY
+    PRINT NEWLINE
+    PRINT correct
+
+    
+    ;print number of correct answers 
+    mov cl, bl
+    add bl, 48     ; number to ascii conversion
+    MOV ah,02H
+    mov dl,bl
+    INT 21H
+    PRINT NEWLINE
+      
+    cmp cl ,3
+    je allok
+    cmp cl ,0
+    je notallok
+    jne ToEnd 
+    
+      
+    allok:
+    PRINT allright
+    jmp ToEnd
+    
+    notallok:
+    PRINT noright
+    jmp ToEnd
+       
+         
+    main endp
+jmp ToEnd
+              
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+UserInputY proc
+    
+    mov ah, 1
+    int 21h
+    cmp al, 'y'
+    je incrementBL
+        
+    ret     
+    UserInputY endp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+UserInputN proc
+    
+    mov ah, 1
+    int 21h
+    cmp al, 'n'
+    je incrementBL
+        
+    ret     
+    UserInputN endp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+incrementBL proc
+    
+    inc bl  ; correct answer for q1
+    
+    ret  
+    incrementBL endp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ToEnd:
+.Exit
+END
